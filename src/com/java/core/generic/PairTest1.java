17ed58d1dbad2b067d2caea8a00facfd4755126d
CatchTheDog <1924528451@qq.com>
2018-11-08 16:58:31 +0800

    泛型方法和泛型类定义 提交。

--- src/com/java/core/generic/PairTest1.java
+++ src/com/java/core/generic/PairTest1.java
@@ -71,18 +71,37 @@ class ArrayAlg {
  * @param <T> 泛型类型参数
  * @author Mr.X
  * @version 1.1
+ * 泛型用来做类型检查使用，避免了部分强制类型转换，且可以实现多态方法；内部使用了类型擦除机制，对于有限定的类型参数，使用第一个参数类型，对于无限定的类型参数，使用Object.
+ * 由于泛型使用类型擦除机制，所以会导致：
+ *      1.
+ *      2.
+ * java对泛型有如下限制：
+ *      1.不应在静态成员中医用封闭类型参数
+ *      2.不能用基本类型实例化泛型类型参数
+ *      3.不能在数据类型转换或instanceof操作中使用“外露”类型参数
+ *      4.不能再new操作中使用外露类型参数
+ *      5.不能再类定义的implements 或 extends 子句中使用外露类型参数
+ * 在方法调用、变量值存取时，系统会自动为泛型域做强制类型转换；
+ *
+ *
  */
 class Pair<T> {
     private T first;
     private T second;
 
+//    private static T third;
+
     public Pair() {
     }
 
-    public Pair(T first, T second) {
-        this.first = first;
-        this.second = second;
-    }
+//    public Pair(T first, T second) {
+//        this.first = first;
+//        this.second = second;
+//    }
+//
+//    private static T testStatic(){
+//        return third;
+//    }
 
     public T getFirst() {
         return first;
