53597f1202ab057a96aeebe05273f02323ce142c
CatchTheDog <1924528451@qq.com>
2018-11-09 17:29:19 +0800

    Swing编程 代码提交。

---
+++ src/com/java/core/guiprogram/NotHelloWorld.java
@@ -0,0 +1,94 @@
+package com.java.core.guiprogram;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Mr.X
+ * @since 1.0.0 2018/11/9 17:11
+ * 在框架中添加组件，显示消息
+ */
+public class NotHelloWorld {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new NotHelloWorldFrame();
+            frame.setTitle("NotHelloWorld");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true
+
+
+            );
+        });
+    }
+
+
+}
+
+/**
+ * A Frame that contains a message panel
+ */
+class NotHelloWorldFrame extends JFrame {
+    public NotHelloWorldFrame() {
+        add(new NotHelloWorldComponent());
+        pack();
+    }
+}
+
+class NotHelloWorldComponent extends JComponent {
+    public static final int MESSAGE_X = 75;
+    public static final int MESSAGE_Y = 100;
+
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    /**
+     * Calls the UI delegate's paint method, if the UI delegate
+     * is non-<code>null</code>.  We pass the delegate a copy of the
+     * <code>Graphics</code> object to protect the rest of the
+     * paint code from irrevocable changes
+     * (for example, <code>Graphics.translate</code>).
+     * <p>
+     * If you override this in a subclass you should not make permanent
+     * changes to the passed in <code>Graphics</code>. For example, you
+     * should not alter the clip <code>Rectangle</code> or modify the
+     * transform. If you need to do these operations you may find it
+     * easier to create a new <code>Graphics</code> from the passed in
+     * <code>Graphics</code> and manipulate it. Further, if you do not
+     * invoker super's implementation you must honor the opaque property,
+     * that is
+     * if this component is opaque, you must completely fill in the background
+     * in a non-opaque color. If you do not honor the opaque property you
+     * will likely see visual artifacts.
+     * <p>
+     * The passed in <code>Graphics</code> object might
+     * have a transform other than the identify transform
+     * installed on it.  In this case, you might get
+     * unexpected results if you cumulatively apply
+     * another transform.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     * @see #paint
+     * @see ComponentUI
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawString("Not a Hello, World program", MESSAGE_X, MESSAGE_Y);
+    }
+
+    /**
+     * If the <code>preferredSize</code> has been set to a
+     * non-<code>null</code> value just returns it.
+     * If the UI delegate's <code>getPreferredSize</code>
+     * method returns a non <code>null</code> value then return that;
+     * otherwise defer to the component's layout manager.
+     *
+     * @return the value of the <code>preferredSize</code> property
+     * @see #setPreferredSize
+     * @see ComponentUI
+     */
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
