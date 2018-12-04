8740e3d25f1bdaea89376581d94e590a1dbd1061
CatchTheDog <1924528451@qq.com>
2018-12-04 16:36:25 +0800

    Swing编程 代码提交。

---
+++ src/com/java/core/extendstest/Employee.java
@@ -0,0 +1,55 @@
+package com.java.core.extendstest;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (null == obj) return false;
+        if (this.getClass() != obj.getClass()) return false;
+        if (!(obj instanceof Employee)) return false;
+        Employee otherObj = (Employee) obj;
+        return this.name.equals(otherObj.getName()) && this.age == otherObj.getAge();
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
